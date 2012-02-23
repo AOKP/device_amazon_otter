83089f6002926c3501c1654882c3cb125118be36
Stephen Duvick <sduvick@gmail.com>
2012-02-23 02:26:06 -0600

    fixing hard mount of sdcard and repo build issues

--- aokp.mk
+++ aokp.mk
@@ -27,7 +27,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
 # Inherit from otter device
 $(call inherit-product, device/amazon/otter/device.mk)
-$(call inherit-product, vendor/aokp/products/common_tablet.mk)
+$(call inherit-product, vendor/aokp/configs/common_tablet.mk)
 $(call inherit-product-if-exists, vendor/amazon/otter/otter-vendor.mk)
 
 # Set those variables here to overwrite the inherited values.
