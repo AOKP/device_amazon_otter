60c359da00d18bb161cedfaac1907855f081bfe4
Steve Spear <stephen.k.spear@gmail.com>
2012-05-18 21:40:34 -0600

    assert exception

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,6 +13,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-add_lunch_combo full_otter-userdebug
-add_lunch_combo cm_otter-userdebug
